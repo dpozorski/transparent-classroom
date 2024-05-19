f40e51fd6b152a9b4bd1d097f9c221e66b7f93ef
Dylan Pozorski <dpozorski@Dylans-MBP.attlocal.net>
2024-05-19 00:02:13 -0500

    Setup.py added.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='transparent-classroom',
+    version='0.0.1',
+    author='Dylan Pozorski',
+    author_email='dylanpozorski@gmail.com',
+    url='https://github.com/dpozorski/transparent-classroom',
+    description="Python Client for accessing Transparent Classroom's data model.",
+    packages=find_packages(exclude=['tests']),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'pytz',
+        'requests'
+    ]
+)
